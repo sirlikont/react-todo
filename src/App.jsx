3c40ef79a627cbd63aebdc90f4699f41fad68d31
Sirli Kont <sirlikont@gmail.com>
2026-01-30 19:16:26 +0200

    lisan asju, kuvan listi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([
+    { id: 1, name: "Piim", isDone: false },
+    { id: 2, name: "Leib", isDone: true },
+    { id: 3, name: "Munad", isDone: false },
+  ]);
+
+  let nextId = items.length + 1;
+
+  function addItem() {
+    if (newItem.trim() === "") return;
+
+    setItems([...items, { id: nextId, name: newItem.trim(), isDone: false }]);
+
+    setNewItem("");
+  }
+
   return (
     <div className="container">
       <h1>Todo App</h1>
+
+      <input
+        type="text"
+        placeholder="Add item"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && addItem()}
+      />
+
+      <button onClick={addItem}>Add item</button>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
